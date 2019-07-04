fb5cb1c13b5b93b6eb1124f5d5fb47d72a305104
Matus Ujhelyi <ujhelyi.m@gmail.com>
2019-07-05 00:49:41 +0200

    add time configs
    Signed-off-by: Matus Ujhelyi <ujhelyi.m@gmail.com>

---
+++ blue-app-configs/time-configs/set-system-time-from-rtc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xv
+T=$(./rtc_manipulate.py get | cut -f 2 -d = | head -c 12)
+echo $T
+YY=20$(echo $T | head -c 2)
+MONTH=$(echo $T | head -c 4 | tail -c 2)
+DD=$(echo $T | head -c 6 | tail -c 2)
+HH=$(echo $T | head -c 8 | tail -c 2)
+MM=$(echo $T | head -c 10 | tail -c 2)
+SS=$(echo $T | head -c 12 | tail -c 2)
+
+TO_SET=$(date --date="TZ=\"UTC\" $YY-$MONTH-$DD $HH:$MM:$SS" +"%F %T")
+echo $TO_SET
+timedatectl set-ntp false
+timedatectl set-time "$TO_SET"
+timedatectl set-ntp true
+
---
+++ blue-app-configs/time-configs/set-time-to-rtc.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./rtc_manipulate.py set $(date --utc +%y%m%d%H%M%S)
