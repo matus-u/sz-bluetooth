c37fd9738bbbba2b27dbc25824cd4e0ecdc01cf8
Matus Ujhelyi <ujhelyi.m@gmail.com>
2020-07-08 22:51:41 +0200

    add images for EN language
    Signed-off-by: Matus Ujhelyi <ujhelyi.m@gmail.com>

--- blue-app/services/LangBasedSettings.py
+++ blue-app/services/LangBasedSettings.py
@@ -15,6 +15,8 @@ class LangBasedSettings:
         self.reloadLanguage()
         if (self.lang) == AppSettings.LanguageList[1]:
             return QtCore.QFile(":/dynamic-images/HU/fortune-style.qss")
+        if (self.lang) == AppSettings.LanguageList[0]:
+            return QtCore.QFile(":/dynamic-images/EN/fortune-style.qss")
 
         return QtCore.QFile(":/dynamic-images/SK/fortune-style.qss")
 
